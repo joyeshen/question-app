8051927beee447e41687355b0fc0407ff7e3509a
Joye Shen <joyeshen05@gmail.com>
2023-07-28 12:57:20 -0700

    Initial Commit

---
+++ question app/question_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  question_appApp.swift
+//  question app
+//
+//  Created by Joye Shen on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct question_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
